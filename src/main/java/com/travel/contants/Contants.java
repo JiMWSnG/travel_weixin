f4b00bd8e628f3511a6264ccd774df72bbeaa38e
wangji <544715907@qq.com>
2016-07-12 15:59:59 +0800

    2016.7.12 项目重构

---
+++ src/main/java/com/travel/contants/Contants.java
@@ -0,0 +1,11 @@
+package com.travel.contants;
+
+public class Contants {
+	/**
+	 * route的type
+	 */
+	public static final String ROUTE_TYPE_RECOMMEND = "recommend";
+	public static final String ROUTE_TYPE_NORMAL  = "normal";
+	public static final String ROUTE_TYPE_HOT = "hot";
+
+}
